0baeb184235b41d6b64707233da3a5d249b9d402
maulana01 <maulanaimammalik4@gmail.com>
2020-10-16 11:26:18 +0700

    new commit

---
+++ src/if1/pkg10119010/latihan5/kambingglobal/KambingGlobal.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if1.pkg10119010.latihan5.kambingglobal;
+
+/**
+ *
+ * @author
+ * NAMA     : Maulana Imam Malik
+ * KELAS    : IF1
+ * NIM      : 10119010
+ * Deskripsi Program : Program ini berisi program untuk menampilkan hasil dari
+ *                     operasi penjumlahan variabel global
+ */
+public class KambingGlobal {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    // Variabel jumlahKambing menjadi variabel instance
+    
+    int jumlahKambing = 88;
+    
+    // Method untuk menampilkan jumlah kambing
+    public void getJumlahKambing() {
+        System.out.println("Jumlah Kambing: " + jumlahKambing);
+    }
+    
+    public void tambahKambing() {
+        jumlahKambing = jumlahKambing + 5;
+        System.out.println("Jumlah kambing setelah ditambah: " + jumlahKambing);
+    }
+    
+    public static void main(String[] args) {
+        KambingGlobal kambingSusu = new KambingGlobal();
+        // TODO code application logic here
+        
+        // Menampilkan jumlah kambing yang ada saat program pertama x berjalan
+        kambingSusu.getJumlahKambing();
+        
+        // Menambah satu kambing
+        kambingSusu.tambahKambing();
+        
+        // Menambah jumlah kambing yang ada
+        kambingSusu.getJumlahKambing();
+    }
+    
+}
